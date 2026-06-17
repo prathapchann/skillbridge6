81c7c0b79c06c9ca41ab86b67e5499a70c27c363
prathapchann <2403717620521035@cit.edu.in>
2026-06-17 21:21:59 +0530

    Update db.js

--- skillbridge-backend/config/db.js
+++ skillbridge-backend/config/db.js
@@ -1,21 +1,24 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  try {
+    const mongoUri = process.env.MONGO_URI;
 
-    try {
-
-        await mongoose.connect(
-            "mongodb+srv://prathap:abcd@bridge.ketqs4o.mongodb.net/?appName=bridge"
-        );
-
-        console.log("MongoDB Connected");
-
-    } catch (error) {
-
-        console.log(error);
-
-        process.exit(1);
+    if (!mongoUri) {
+      console.error("MONGO_URI is not defined. Set it in Render environment variables.");
+      process.exit(1);
     }
+
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
