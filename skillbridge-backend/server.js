3f7f5e2a7331ff2f1b56ff6d3a377a635640d140
prathapchann <2403717620521035@cit.edu.in>
2026-06-17 21:22:37 +0530

    Update server.js

--- skillbridge-backend/server.js
+++ skillbridge-backend/server.js
@@ -21,10 +21,12 @@ app.use(express.json());
 
 app.use("/api/users", require("./routes/studentRoutes"));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/',()=>{console.log("tested")})
+app.get("/", (req, res) => {
+  res.send("SkillBridge backend is running");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
